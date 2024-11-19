a4f3dc4aca595f2490b9a88a164159a17ec8059d
brandonvidal93 <brandonvidal93@gmail.com>
2024-11-19 12:33:32 -0500

    config app.js

---
+++ js/app.js
@@ -0,0 +1,69 @@
+// Importar métodos del api.js
+import { getProducts, getProductByID, updateProduct, deleteProduct } from "./api";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const productList = document.getElementById("product-list");
+
+  const products = await getProducts();
+  productList.innerHTML = products.map(product => `
+      <div class="col-xs-12 col-sm-6 col-md-3 card">
+        <img class="card-img-top" src="${product.imgUrl}">
+        <div class="card-body d-flex flex-column justify-content-end">
+          <h5 class="card-title">${product.name}</h5>
+          <p class="card-text">${product.price}</p>
+          <a onclick="viewProduct(${product.id})" class="btn btn-primary">Ver más</a>
+        </div>
+      </div>
+    `).join("");
+});
+
+// Método para ver los detalles del producto cuando damos clic en el botón "Ver más"
+window.viewProduct = async (id) => {
+  const product = await getProductByID(id);
+
+  const productDetails = `
+    <div class="col">
+      <img class="card-img-top" src="${product.imgUrl}">
+      <h3>${product.name}</h3>
+      <p>${product.description}</p>
+      <p>Precio: ${product.price}</p>
+      <button class="btn btn-warning" onclick="enableEdit(${product.id})">Editar</button>
+      <button class="btn btn-danger" onclick="deleteProduct(${product.id})">Eliminar</button>
+    </div>
+  `;
+  document.getElementById("product-list").innerHTML = productDetails;
+};
+
+// Vista para editar la información
+window.enableEdit = async (id) => {
+  const product = await getProductByID(id);
+
+  const editForm = `
+    <div class="row gap-3">
+      <input type="text" id="name" value="${product.name}">
+      <textarea id="description">${product.description}</textarea>
+      <input type="number" id="price" value="${product.price}">
+      <input type="text" id="imgUrl" value="${product.imgUrl}">
+      <button class="btn btn-success" onclick="saveEdit(${id})">Guardar</button>
+    </div>
+  `;
+  document.getElementById("product-list").innerHTML = editForm;
+};
+
+// Guardar la información actualizada
+window.saveEdit = async (id) => {
+  const productUpdate = {
+    name: document.getElementById("name").value,
+    description: document.getElementById("description").value,
+    price: parseFloat(document.getElementById("price").value),
+    imgUrl: document.getElementById("imgUrl").value
+  };
+  await updateProduct(id, productUpdate);
+  location.reload(); // Recarga la página para ver los cambios
+};
+
+// Borrar el producto seleccionado
+window.deleteProduct = async (id) => {
+  await deleteProduct(id);
+  location.reload(); // Recarga la página para ver los cambios
+}
